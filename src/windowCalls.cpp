f46cb42ec2488365de69062ee172deebd08ba783
K2rlXYZ <karl111@protonmail.com>
2024-04-05 16:29:47 +0300

    Menu system

--- src/windowCalls.cpp
+++ src/windowCalls.cpp
@@ -1,13 +1,22 @@
 #include <windows.h>
+#include <resource.h>
 
 const char g_szClassName[] = "myWindowClass";
-
 
 // Step 3: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
+        case WM_LBUTTONDOWN:
+        {
+            char szFileName[MAX_PATH];
+            HINSTANCE hInstance = GetModuleHandle(NULL);
+
+            GetModuleFileName(hInstance, szFileName, MAX_PATH);
+            MessageBox(hwnd, szFileName, "This program is:", MB_OK | MB_ICONINFORMATION);
+        }
+        break;
         case WM_CLOSE:
             DestroyWindow(hwnd);
         break;
@@ -29,12 +38,12 @@ WNDCLASSEX CreateDefaultWNDCLASSEX(HINSTANCE hInstance){
     wc.cbClsExtra    = 0;
     wc.cbWndExtra    = 0;
     wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIcon         = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = NULL;
+    wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
     wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm       = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
     return wc;
 }
 
